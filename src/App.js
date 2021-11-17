15168ab7b31906c5e248d5e78ea58ba6432a90e9
huyvouit <huyvouit@gmail.com>
2021-11-17 12:58:40 +0700

    setup file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 //component
 import Sidebar from "./Component/sidebar";
 import RoutesSideBar from "./Component/route";
+import LoginScreen from "./Component/login_screen";
 //screen
 
 import { BrowserRouter, Route } from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
             <div className="layout__content">
               <div className="layout__content-main">
                 <RoutesSideBar />
+                <Route path="/login" exact component={LoginScreen} />
               </div>
             </div>
           </div>
